dd385f594c0e515edf52416db9442d9ba864a1e3
SoloAs <sunny.alexander@mail.ru>
2018-11-13 05:51:29 -0500

    moved pcap from node_modules, might need to do node-gyp configure build on installation

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
-var pcap = require('pcap')
+var pcap = require('./pcap')
 var parser = require('pcap-parser')
 var util = require('util')
-console.log('hello')
-console.log(util.inspect({a:1, b:"b"}, false,2,true));
+console.log('Started capturing')
 pcap_session = pcap.createSession()
 pcap_session.on('packet', raw_packet => {
   var packet = pcap.decode.packet(raw_packet)
-  
-    if (packet.payload.ethertype ==  '0x88cc') 
-    {
-      console.log('FOUND LLDP PACKET')
-      //console.log(packet.payload.ethertype)
-      var optTlvs = packet.payload.payload.optionalTlvs
-      optTlvs.forEach(element => {
-        switch(element.tlvType) {
-          case 4: console.log("port desc: " + element.portDescription); break
-          case 5: console.log("OS: " + element.systemName); break
-          case 6: console.log("OS desc: " + element.systemDescription); break
-          case 1: console.log("addr: " + element.addr); break
-          case 2: console.log("intString: " + element.intString); break
-          case 3: console.log("ttl: " + element.ttlSeconds)
-        }
-      });
-    }
-  // var info = util.inspect(packet, true, 2, true)
-  // console.log(JSON.parse(info))
- // console.log(packet.link.ip.tcp.port)
+  if (packet.payload.ethertype ==  '0x88cc') 
+  {
+    console.log('FOUND LLDP PACKET:')
+//    console.log(packet)
+    console.log('PACKET DESCRIPTION:')
+    console.log(packet.payload.payload)
+  }
 })
 
 
-// PcapPacket {
-//   link_type: 'LINKTYPE_ETHERNET',
-//   pcap_header: PcapHeader { tv_sec: 1528285440, tv_usec: 331146, caplen: 60, len: 60 },
-//   payload:
-//    EthernetPacket {
-//      emitter: undefined,
-//      dhost: EthernetAddr { addr: [Object] },
-//      shost: EthernetAddr { addr: [Object] },
-//      ethertype: 2054,
-//      vlan: null,
-//      payload:
-//       Arp {
-//         emitter: undefined,
-//         htype: 1,
-//         ptype: 2048,
-//         hlen: 6,
-//         plen: 4,
-//         operation: 1,
-//         sender_ha: [Object],
-//         sender_pa: [Object],
-//         target_ha: [Object],
-//         target_pa: [Object] } },
-//   emitter: undefined }
